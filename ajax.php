4b27b8911a9509640935962b58e9e3535599948a
JP <fantasma11747@gmail.com>
2023-03-13 07:17:36 -0300

    Commit inicial

---
+++ ajax.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <title>ATV 3 AJAX</title>
+
+    <script>
+        function procurarCidade() {
+            let nome = document.getElementById("estadoID").value;
+            $.ajax({
+                url: "retornaCidades.php",
+                type: "POST",
+                data: "estado=" + nome,
+                dataType: "html"
+            }).done(function(resposta) {
+                console.log(resposta);
+                $('#cidadeID').html(resposta);
+            }).fail(function(jqXHR, textStatus) {
+                console.log("Request failed: " + textStatus);
+            }).always(function() {
+                console.log("completou");
+            });
+        }
+    </script>
+
+
+
+
+
+
+
+
+
+</head>
+
+<body>
+    <form id="formDadastroDeAluno" method="POST" action="ajax.php">
+        <select id="estadoID" onchange="procurarCidade()" name="estado" class="form-control">
+            <option value="0" selected>Escolha uma cidade...</option>
+            <option value="sp">São Paulo</option>
+            <option value="ms">Mato Grosso do Sul</option>
+        </select>
+    </form>
+
+
+    <select id="cidadeID" name="cidade" class="form-control">
+
+    </select>
+
+
+
+
+
+
+</body>
+
+</html>
